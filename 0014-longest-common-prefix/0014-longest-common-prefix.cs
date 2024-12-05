eb6a4cf923d62af43d4285d2fde3f0898ff035ce
Lucas Venturella <lucas.venturella@hotmail.com>
2024-12-05 14:22:13 -0300

    Time: 8 ms (15.94%), Space: 44.3 MB (9.72%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cs
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cs
@@ -1,25 +1,15 @@
 public class Solution {
     public string LongestCommonPrefix(string[] strs) {
-        // Sort the array of strings
-        Array.Sort(strs);
-        
-        // Use StringBuilder to store the common prefix
-        StringBuilder sb = new StringBuilder();
-        
-        // Get the first and last strings in the sorted array
-        string first = strs[0];
-        string last = strs[strs.Length - 1];
-        
-        // Compare characters of the first and last strings
-        for (int i = 0; i < Math.Min(first.Length, last.Length); i++)
+        string prefix = strs[0];
+        for(int i = 1; i < strs.Length; i++)
         {
-            if (first[i] != last[i])
+            while(strs[i].IndexOf(prefix) != 0)
             {
-                return sb.ToString();
+                prefix = prefix.Substring(0, prefix.Length-1);
+                if(prefix == "")
+                    return "";
             }
-            sb.Append(first[i]);
         }
-        
-        return sb.ToString();
+        return prefix;
     }
 }
